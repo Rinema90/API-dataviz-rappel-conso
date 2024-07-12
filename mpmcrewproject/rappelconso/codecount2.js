71def0740721ac8c7966e80117c51084c4d2bba9
Marine Suhard <marine.suhard@gmail.com>
2024-07-12 16:33:36 +0200

    sort and rename all the files of the project :zap:

--- mpmcrewproject/rappelconso/codecount2.js
+++
@@ -1,7 +0,0 @@
-    const total = Object.values(newResultsCategorie).reduce((acc, curr) => acc + curr, 0)
-        const percentages = {};
-        for (const key in newResultsCategorie) {
-            percentages[key] = ((newResultsCategorie[key] / total) * 100).toFixed(2) + "%";
-        }
-
-        console.log("percentages", percentages);
